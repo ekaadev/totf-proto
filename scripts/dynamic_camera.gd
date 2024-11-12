e3cc1e0ccd8daf50568ab042c411d2e9973dd6bf
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-11-13 02:15:42 +0700

    feat: add dynamic camera

--- scripts/dynamic_camera.gd
+++ scripts/dynamic_camera.gd
@@ -1,22 +1,22 @@
 extends Camera2D
-#
-#var MAX_DISTANCE = 48
-#
-#var target_distance = 0
-#var center_pos = position
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#var direction = center_pos.direction_to(get_local_mouse_position())
-	#var target_pos = center_pos + direction * target_distance
-	#
-	#target_pos = target_pos.clamp(
-		#center_pos - Vector2(MAX_DISTANCE, MAX_DISTANCE),
-		#center_pos + Vector2(MAX_DISTANCE, MAX_DISTANCE)
-	#)
-	#
-	#position = target_pos
-#
-#func _input(event: InputEvent) -> void:
-	#if event is InputEventMouseMotion:
-		#target_distance = center_pos.distance_to(get_local_mouse_position()) / 2
+
+var MAX_DISTANCE = 32
+
+var target_distance = 0
+var center_pos = position
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction = center_pos.direction_to(get_local_mouse_position())
+	var target_pos = center_pos + direction * target_distance
+	
+	target_pos = target_pos.clamp(
+		center_pos - Vector2(MAX_DISTANCE, MAX_DISTANCE),
+		center_pos + Vector2(MAX_DISTANCE, MAX_DISTANCE)
+	)
+	
+	position = target_pos
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		target_distance = center_pos.distance_to(get_local_mouse_position()) / 2
