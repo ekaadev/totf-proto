eaf6d21deea9e8b7a8848e381b3d2ace682a8947
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-11-16 14:02:09 +0700

    feat: implement enemy hitbox and hurtbox components; update enemy scene

--- scripts/HitboxComponent.gd
+++ scripts/HitboxComponent.gd
@@ -1,14 +1,8 @@
 extends Area2D
 class_name HitboxComponent
 
-signal hit(damage)
-
 @export var damage: int = 10
 
-func _ready():
-	connect("body_entered", Callable(self, "_on_body_entered"))
-
-func _on_body_entered(body):
-	if body.is_in_group("enemies"):
-		body.take_damage(damage)
-		emit_signal("hit", damage)
+func _init() -> void:
+	collision_layer = 1
+	collision_mask = 0
--- scripts/HurtboxComponent.gd
+++ scripts/HurtboxComponent.gd
@@ -1,12 +1,18 @@
 extends Area2D
 class_name HurtboxComponent
 
-func _ready():
-	connect("body_entered", Callable(self, "_on_body_entered"))
+func _init() -> void:
+	collision_layer = 0
+	collision_mask = 1
 
-func _on_body_entered(hitbox: HitboxComponent) -> void:
+
+func _ready() -> void:
+	connect("area_entered", Callable(self, "_on_area_entered"))
+
+func _on_area_entered(hitbox: HitboxComponent) -> void:
 	if hitbox == null:
-		return
+		print("Hitbox is null")
 	
 	if owner.has_method("take_damage"):
 		owner.take_damage(hitbox.damage)
+	
