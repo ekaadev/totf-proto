678c27846eb146d21500f53bc10fa3459c2f5f0e
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-11-19 00:59:08 +0700

    feat: add attack animations for all directions; update hitbox properties and integrate animation player for basic attacks

--- scripts/player/basic_attack_state.gd
+++ scripts/player/basic_attack_state.gd
@@ -17,15 +17,19 @@ func _on_next_transitions() -> void:
 func _on_enter() -> void:
 	if player.player_direction == Vector2.UP:
 		animation_sprite.play("attack_spear_up")
+		animation_player.play("attack_up")
 	elif player.player_direction == Vector2.DOWN:
 		animation_sprite.play("attack_spear_down")
 		animation_player.play("attack_down")
 	elif player.player_direction == Vector2.LEFT:
 		animation_sprite.play("attack_spear_left")
+		animation_player.play("attack_left")
 	elif player.player_direction == Vector2.RIGHT:
 		animation_sprite.play("attack_spear_right")
+		animation_player.play("attack_right")
 	else:
 		animation_sprite.play("attack_spear_down")
+		animation_player.play("attack_down")
 
 func _on_exit() -> void:
 	animation_sprite.stop()
