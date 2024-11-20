be71f70c36f115e85a8c26a5b9c0e5688285cec5
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-11-20 07:13:37 +0700

    feat: update player attack animations and hitbox properties; adjust player scene positioning and implement spear attack transition

--- scripts/player/walk_state.gd
+++ scripts/player/walk_state.gd
@@ -34,6 +34,9 @@ func _on_next_transitions() -> void:
 	if !GameInputEvents.is_movement_input():
 		transition.emit("Idle")
 
+	if player.current_tool == DataTypes.Tools.Spear && GameInputEvents.use_tool():
+		transition.emit("BasicAttack")
+
 func _on_enter() -> void:
 	pass
 
