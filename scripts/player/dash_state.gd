0c4a15ac472d2fc6bcaee57689228e2b37b6da29
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-11-26 22:39:43 +0700

    feat: add dash state for player; implement dash state script and update player scene

---
+++ scripts/player/dash_state.gd
@@ -0,0 +1,19 @@
+extends NodeState
+
+@export var player: Player
+@export var animation_sprite: AnimatedSprite2D
+
+func _on_process(_delta : float) -> void:
+	pass
+
+func _on_physics_process(_delta: float) -> void:
+	pass
+
+func _on_next_transitions() -> void:
+	pass
+
+func _on_enter() -> void:
+	pass
+
+func _on_exit() -> void:
+	pass
