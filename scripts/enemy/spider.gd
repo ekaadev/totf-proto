74fc9af18088dcaa3f1697e004bcf0ab825e6f35
Actually IAmPlayer <70600266+NotIAmPlayer@users.noreply.github.com>
2024-12-19 11:15:48 +0700

    spider part 2

--- scripts/enemy/spider.gd
+++ scripts/enemy/spider.gd
@@ -7,7 +7,7 @@ extends CharacterBody2D
 var direction: Vector2
 var distance: Vector2
 
-var scatterPhases = 16
+var scatterPhases = 32
 
 var dirArray = [Vector2(-1, 0), Vector2(1, 0), Vector2(0, -1), Vector2(0, 1)]
 
@@ -18,7 +18,7 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if scatterPhases > 0:
-		if direction.y == 0 and abs(distance.x) >= 32:
+		if direction.y == 0 and abs(distance.x) >= 8:
 			scatterPhases -= 1 if scatterPhases > 0 else 0
 			
 			if scatterPhases > 0:
@@ -29,7 +29,7 @@ func _process(delta: float) -> void:
 				
 				direction = tempDir
 				distance = Vector2.ZERO
-		elif direction.x == 0 and abs(distance.y) >= 32:
+		elif direction.x == 0 and abs(distance.y) >= 8:
 			scatterPhases -= 1 if scatterPhases > 0 else 0
 			
 			var tempDir = -direction
@@ -41,25 +41,37 @@ func _process(delta: float) -> void:
 			distance = Vector2.ZERO
 	else:
 		if (direction.y == 0 and abs(distance.x) >= 64) or (direction.x == 0 and abs(distance.y) >= 64):
-			var tempDir = player.position - position
+			var randomNoise = randf()
 			
-			if abs(tempDir.x) >= abs(tempDir.y):
-				# move on the X axis
+			if randomNoise > 0.8:
+				scatterPhases += randi_range(1, 5) * 4
 				
-				if tempDir.x > 0:
-					tempDir = dirArray[1]
-				elif tempDir.x < 0:
-					tempDir = dirArray[0]
+				var tempDir = -direction
+				
+				while tempDir == -direction:
+					tempDir = dirArray.pick_random()
+				
+				direction = tempDir
+				distance = Vector2.ZERO
 			else:
-				# move on the Y axis
+				var tempDir = player.position - position
 				
-				if tempDir.y > 0:
-					tempDir = dirArray[3]
-				elif tempDir.y < 0:
-					tempDir = dirArray[2]
-			
-			direction = tempDir
-			distance = Vector2.ZERO
+				if abs(tempDir.x) >= abs(tempDir.y):
+					# move on the X axis
+					if tempDir.x > 0:
+						tempDir = dirArray[1]
+					elif tempDir.x < 0:
+						tempDir = dirArray[0]
+				else:
+					# move on the Y axis
+					
+					if tempDir.y > 0:
+						tempDir = dirArray[3]
+					elif tempDir.y < 0:
+						tempDir = dirArray[2]
+				
+				direction = tempDir
+				distance = Vector2.ZERO
 	
 	if abs(direction.y) > abs(direction.x):
 		if direction.y > 0:
@@ -75,7 +87,7 @@ func _process(delta: float) -> void:
 		sprite.flip_h = false
 
 func _physics_process(delta: float) -> void:
-	velocity = direction.normalized() * 200
+	velocity = direction.normalized() * 180
 	move_and_collide(velocity * delta)
 	
 	distance += velocity * delta
