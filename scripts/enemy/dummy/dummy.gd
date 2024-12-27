04d1e3c6503e5dfb965051a32c200b2c423dc148
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-12-27 16:10:35 +0700

    feat: add hit flash animation and shader to dummy enemy for damage indication

--- scripts/enemy/dummy/dummy.gd
+++ scripts/enemy/dummy/dummy.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 @onready var damage_number_origin = $DamageNumbersOrigin
+@onready var animated_player = $AnimationPlayer
 
 func take_damage(total_damage: float) -> void:
 	print("Take Damage ", total_damage)
+	animated_player.play("hit_flash")
 	DamageNumbers.display_damage_number(total_damage, damage_number_origin.global_position)
 	
