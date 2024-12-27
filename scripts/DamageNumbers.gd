62f1e4219701bfc19d24124df883ac49b11b8d1f
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-12-27 15:09:56 +0700

    feat: implement damage number display for enemy interactions

---
+++ scripts/DamageNumbers.gd
@@ -0,0 +1,38 @@
+extends Node
+
+func display_damage_number(total_damage: float, position: Vector2) -> void:
+	var number = Label.new()
+
+	number.global_position = position
+	number.text = str(total_damage)
+	number.z_index = 5
+	number.label_settings = LabelSettings.new()
+
+	var color = "#e7d5b3"
+
+	number.label_settings.font = load("res://assets/fonts/NicoBold-Regular.ttf")
+	number.label_settings.font_color = color
+	number.label_settings.font_size = 16
+	number.label_settings.outline_color = "#000"
+	number.label_settings.outline_size = 5
+
+	call_deferred("add_child", number)
+
+	await number.resized
+	number.pivot_offset = Vector2(number.size / 2)
+
+	var tween = get_tree().create_tween()
+	tween.set_parallel(true)
+
+	tween.tween_property(
+		number, "position:y", number.position.y - 24, 0.25
+	).set_ease(Tween.EASE_OUT)
+	tween.tween_property(
+		number, "position:y", number.position.y, 0.5
+	).set_ease(Tween.EASE_IN).set_delay(0.25)
+	tween.tween_property(
+		number, "scale", Vector2.ZERO, 0.25
+	).set_ease(Tween.EASE_IN).set_delay(0.5)
+
+	await tween.finished
+	number.queue_free()
--- scripts/enemy/dummy/dummy.gd
+++ scripts/enemy/dummy/dummy.gd
@@ -1,4 +1,8 @@
 extends CharacterBody2D
 
+@onready var damage_number_origin = $DamageNumbersOrigin
+
 func take_damage(total_damage: float) -> void:
 	print("Take Damage ", total_damage)
+	DamageNumbers.display_damage_number(total_damage, damage_number_origin.global_position)
+	
