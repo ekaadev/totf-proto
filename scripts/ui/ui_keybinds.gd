9fe1a78c0bcd0301247b12b97a1465cf27c0c164
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-12-29 01:43:34 +0700

    feat: adjust UI keybinds scenes; add texture card for buff

--- scripts/ui/ui_keybinds.gd
+++ scripts/ui/ui_keybinds.gd
@@ -6,21 +6,21 @@ extends CanvasLayer
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if state.current_node_state_name == "idle" || state.current_node_state_name == "basicattack" || state.current_node_state_name == "slashattack":
-		ui_keybind_idle()
-	else:
+	if state.current_node_state_name.to_lower() == "walk":
 		ui_keybind_not_idle()
+	else:
+		ui_keybind_idle()
 
 func ui_keybind_idle() -> void:
 	var tween = create_tween()
 	tween.tween_property(
 		layout_keybinds, "position:y", 658, 0.5
-	).set_ease(Tween.EASE_IN).set_delay(0.05)
+	).set_ease(Tween.EASE_IN_OUT).set_delay(0.05)
 	await tween.finished
 
 func ui_keybind_not_idle() -> void:
 	var tween = create_tween()
 	tween.tween_property(
 		layout_keybinds, "position:y", layout_keybinds.global_position.y + 62, 0.5
-	).set_ease(Tween.EASE_OUT).set_delay(0.05)
+	).set_ease(Tween.EASE_OUT_IN).set_delay(0.5)
 	await tween.finished
