4d7f41917617ff70a9f07a98d9fe319a56490040
Yohanes Eka Putra <ekaadev9@gmail.com>
2025-01-12 04:52:06 +0700

    feat: enhance game pause UI with additional animations and information labels

--- scripts/ui/ui_game_pause.gd
+++ scripts/ui/ui_game_pause.gd
@@ -1,13 +1,22 @@
 extends CanvasLayer
 
+@onready var timer = $Timer
+@onready var pause_animation = $PauseAnimation
 @onready var resume_button = $MarginContainer/HBoxContainer/VBoxContainer/ResumeButton
+@onready var info_label = $MarginContainer3/VBoxContainer/InformationLabel
 
 func _ready() -> void:
 	resume_button.grab_focus()
 
 func _on_timer_timeout() -> void:
-	$PauseAnimation.play("pause_in")
-	await $PauseAnimation.animation_finished
-	$Timer.stop()
+	pause_animation.play("pause_in")
+	await pause_animation.animation_finished
+	timer.stop()
 	# $PauseAnimation.play("pause_out")
 	# await $PauseAnimation.animation_finished
+
+func _on_resume_button_focus_entered() -> void:
+	info_label.text = "INFO: Resume the game"
+
+func _on_goto_main_menu_button_focus_entered() -> void:
+	info_label.text = "INFO: Go back to the main menu"
